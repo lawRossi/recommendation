982b840a757770794d5df0dd4d214fd94900126d
Rossi <luoweiang@sina.cn>
2021-03-06 17:20:37 +0800

    update

--- recommendation/dataset/recall.py
+++ recommendation/dataset/recall.py
@@ -7,55 +7,16 @@ import struct
 import os
 import numpy as np
 from tqdm import tqdm
+import random
 
 
 class RecallDataset(data.Dataset):
-    def __init__(self, click_list_file, attr_file=None, min_history=3, max_history=30, 
-            cache_path=".recall", rebuild_cache=False, min_count=3):
-        super().__init__()
+    def __init__(self,click_list_file, attr_file, min_count):
         self.click_list_file = click_list_file
         self.attr_file = attr_file
-        self.min_history = min_history
-        self.max_history = max_history
         self.min_count = min_count
-        if not os.path.exists(cache_path) or rebuild_cache:
-            shutil.rmtree(cache_path, ignore_errors=True)
-            self._build_cache(cache_path)
-        self.env = lmdb.open(cache_path, create=False, lock=False, readonly=True)
-        with self.env.begin(write=False) as txn:
-            self.length = txn.stat()["entries"] - 1
-            vocab, attr_vocabs, weights = pickle.loads(txn.get(b"components"))
-            self.vocab = vocab
-            self.attr_vocabs = attr_vocabs
-            self.weights = weights
-
-    def __len__(self) -> int:
-        return self.length
     
-    def __getitem__(self, index: int):
-        with self.env.begin(write=False) as txn:
-            user_id, history, positive = pickle.loads(txn.get(struct.pack(">I", index)))
-            history = [self.vocab.get(content_id, len(self.vocab)+1) for content_id in history]
-            history_size = len(history)
-            history = history + [0] * (self.max_history - len(history))
-            positive = self.vocab.get(positive)
-            values = [np.array(history), history_size, positive]
-            if self.attr_vocabs:
-                attrs = self.attributes.get(user_id)
-                attr_values = [vocab.get(attrs[attr_name], len(vocab)+1) for attr_name, vocab in self.attr_vocabs.items()]
-                values.extend(attr_values)
-            return tuple(values)
-
-    def _load_attributes(self):
-        with open(self.attr_file, encoding="utf-8") as fi:
-            headers = fi.readline().split("\t")[1:]  # skip id
-            self.attribute_names = headers
-            self.attributes = {}
-            for line in fi:
-                splits = line.strip().split("\t")
-                self.attributes[splits[0]] = (dict(zip(headers, splits[1:])))
-
-    def _build_vocab_builary(self):
+    def _build_vocabulary(self):
         with open(self.click_list_file) as fi:
             counts = defaultdict(int)
             for line in fi:
@@ -83,10 +44,19 @@ class RecallDataset(data.Dataset):
         else:
             self.attr_vocabs = None
 
+    def _load_attributes(self):
+        with open(self.attr_file, encoding="utf-8") as fi:
+            headers = fi.readline().split("\t")[1:]  # skip id
+            self.attribute_names = headers
+            self.attributes = {}
+            for line in fi:
+                splits = line.strip().split("\t")
+                self.attributes[splits[0]] = (dict(zip(headers, splits[1:])))
+
     def _build_cache(self, cache_path):
         if self.attr_file:
             self._load_attributes()
-        self._build_vocab_builary()
+        self._build_vocabulary()
         with lmdb.open(cache_path, map_size=int(1e11)) as env:
             with env.begin(write=True) as txn:
                 txn.put(b"components", pickle.dumps((self.vocab, self.attr_vocabs, self.weights)))
@@ -94,13 +64,50 @@ class RecallDataset(data.Dataset):
                     for key, value in buffer:
                         txn.put(key, value)
 
+
+class YoutubeNetDataset(RecallDataset):
+    def __init__(self, click_list_file, attr_file=None, min_history=3, max_history=30, 
+            cache_path=".recall", rebuild_cache=False, min_count=3):
+        super().__init__(click_list_file, attr_file, min_count)
+        self.min_history = min_history
+        self.max_history = max_history
+        if not os.path.exists(cache_path) or rebuild_cache:
+            shutil.rmtree(cache_path, ignore_errors=True)
+            self._build_cache(cache_path)
+        self.env = lmdb.open(cache_path, create=False, lock=False, readonly=True)
+        with self.env.begin(write=False) as txn:
+            self.length = txn.stat()["entries"] - 1
+            vocab, attr_vocabs, weights = pickle.loads(txn.get(b"components"))
+            self.vocab = vocab
+            self.attr_vocabs = attr_vocabs
+            self.weights = weights
+        if self.attr_vocabs:
+            self._load_attributes()
+
+    def __len__(self) -> int:
+        return self.length
+    
+    def __getitem__(self, index: int):
+        with self.env.begin(write=False) as txn:
+            user_id, history, positive = pickle.loads(txn.get(struct.pack(">I", index)))
+            history = [self.vocab.get(content_id, len(self.vocab)+1) for content_id in history]
+            history_size = len(history)
+            history = history + [0] * (self.max_history - len(history))
+            positive = self.vocab.get(positive)
+            values = [np.array(history), history_size, positive]
+            if self.attr_vocabs:
+                attrs = self.attributes.get(user_id)
+                attr_values = [vocab.get(attrs[attr_name], len(vocab)+1) for attr_name, vocab in self.attr_vocabs.items()]
+                values.extend(attr_values)
+            return tuple(values)
+
     def _yield_buffer(self, buffer_size=10000):
         buffer = list()
         idx = 0
         with open(self.click_list_file) as fi:
             pbar = tqdm(fi, mininterval=1, smoothing=0.1)
             pbar.set_description("building cache")
-            for line in fi:
+            for line in pbar:
                 user_id, clicked_ids = line.strip().split("\t")
                 clicked_ids = clicked_ids.split(" ")
                 for i in range(self.min_history, len(clicked_ids)):
@@ -112,4 +119,87 @@ class RecallDataset(data.Dataset):
                     idx += 1
                     if len(buffer) == buffer_size:
                         yield buffer
+                        buffer.clear()
+            yield buffer
+
+
+class GESDataset(RecallDataset):
+    """[summary]
+    """
+    def __init__(self, click_list_file, attr_file, window_size=5, cache_path=".ges", rebuild_cache=False,
+            min_count=3,  num_negative=5):
+        super().__init__(click_list_file, attr_file, min_count)
+        self.window_size = window_size
+        self.num_negative = num_negative
+
+        if rebuild_cache or not os.path.exists(cache_path):
+            shutil.rmtree(cache_path, ignore_errors=True)
+            self._build_cache(cache_path)
+
+        self.env = lmdb.open(cache_path, create=False, lock=False, readonly=True)
+        with self.env.begin(write=False) as txn:
+            self.length = txn.stat()["entries"] - 1
+            vocab, attr_vocabs, weights = pickle.loads(txn.get(b"components"))
+            self.vocab = vocab
+            self.attr_vocabs = attr_vocabs
+            self.items = np.arange(0, len(self.vocab)+1)
+            self.weights = weights
+        if self.attr_vocabs:
+            self._load_attributes()
+
+    def __len__(self) -> int:
+        return self.length
+
+    def _load_attributes(self):
+        with open(self.attr_file, encoding="utf-8") as fi:
+            headers = fi.readline().split("\t")[1:]  # skip id
+            self.attribute_names = headers
+            self.attributes = {}
+            for line in fi:
+                splits = line.strip().split("\t")
+                self.attributes[splits[0]] = (dict(zip(headers, splits[1:])))
+
+    def __getitem__(self, index):
+        with self.env.begin(write=False) as txn:
+            central_id, context = pickle.loads(txn.get(struct.pack(">I", index)))
+            negatives = np.random.choice(self.items, size=len(context)*self.num_negative, p=self.weights)
+            labels = [1] * len(context) + [0] * len(negatives)
+            context = [self.vocab.get(item_id, len(self.vocab)+1) for item_id in context]
+            context.extend(negatives)
+            idxes = list(range(len(context)))
+            random.shuffle(idxes)
+            context = np.array([context[idx] for idx in idxes])
+            labels = np.array([labels[idx] for idx in idxes])
+
+            central_idx = self.vocab[central_id]
+            values = [labels, central_idx, context]
+
+            attrs = []
+            for attr_name in self.attribute_names:
+                attr_value = self.attributes[central_id].get(attr_name)
+                attr_vocab = self.attr_vocabs[attr_name]
+                attrs.append(attr_vocab.get(attr_value, len(attr_vocab)))
+            values.extend(attrs)
+
+            return tuple(values)
+
+    def _yield_buffer(self, buffer_size=1000):
+        buffer = list()
+        idx = 0
+        with open(self.click_list_file) as fi:
+            pbar = tqdm(fi, mininterval=1, smoothing=0.1)
+            pbar.set_description("building cache")
+            for line in pbar:
+                _, clicked_ids = line.strip().split("\t")
+                clicked_ids = clicked_ids.split(" ")
+                for i in range(self.window_size, len(clicked_ids)-self.window_size):
+                    central_id = clicked_ids[i]
+                    if central_id not in self.vocab:
+                        continue
+                    context = clicked_ids[i-self.window_size:i] + clicked_ids[i+1:i+self.window_size]
+                    buffer.append((struct.pack(">I", idx), pickle.dumps((central_id, context))))
+                    idx += 1
+                    if len(buffer) == buffer_size:
+                        yield buffer
+                        buffer.clear()
             yield buffer
